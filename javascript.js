dde430fad11c4e6b11692b2695e7b122e7fa3e83
Alexander Voets <avoets@jaguarlandrover.com>
2019-02-20 12:53:33 -0800

    up to lesson 2.8

--- javascript.js
+++ javascript.js
@@ -2,8 +2,37 @@
 /* Multi
 Line Comments*/ 
 
+document.getElementsByTagName("h1")[0].style.color = "#ff0000";
+
 console.log("hiya friend!");
 
 for (var i = 0; i < 10; i++) {
     console.log(i);
   }
+
+  document.body.addEventListener('click', function () {
+    var myParent = document.getElementById("Banner"); 
+    var myImage = document.createElement("img");
+    myImage.src = 'https://thecatapi.com/api/images/get?format=src&type=gif';
+    myParent.appendChild(myImage);
+    myImage.style.marginLeft = "160px";
+});
+
+//Writing things to the console
+console.log(1 + 5 - 2);
+
+// Numbers in JS are just being typed in. Typing 3 will return 3 to the console.
+3
+3 + 2.1
+// Booleans for comparison are also just typed in
+5 > 10 // returns true
+
+//Strings - strings are in "" and can be numbers or letters; without "" the interpreter things it is a variable and not a string
+//String concatenation
+"Hello" + ", Welcome to New York City"
+
+//Variables
+var variableName = value;
+var name = "Zoe"; 
+var greeting = "Hello";
+greeting + " Mike!";
