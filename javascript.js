bd6cca1bda106c830a8ffa306139458611d55601
Alexander Voets <avoets@jaguarlandrover.com>
2019-02-20 12:17:28 -0800

    adding javascript

---
+++ javascript.js
@@ -0,0 +1,9 @@
+// Link for Developer Tool/ShortCuts in Google Chrome https://developers.google.com/web/tools/chrome-devtools/shortcuts
+/* Multi
+Line Comments*/ 
+
+console.log("hiya friend!");
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+  }
