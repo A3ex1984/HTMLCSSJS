e8fc09097836ee1ea402f67b7902fa1103caf5aa
Alexander Voets <avoets@jaguarlandrover.com>
2019-03-13 17:42:04 -0700

    for loops and increments

--- javascript.js
+++ javascript.js
@@ -143,6 +143,28 @@ while (num >= 1) {
   num = num - 1;
 }
 
+//For loops are the most commonly loops used in JavaScript as it gives you more control over the looping
+for ( start; stop; step ) { // In a for loop I define all these steps up front as oppased to throughout the loop - this gives more control
+  // do this thing
+}
+for (var i = 0; i < 6; i = i + 1){ // x++ is the same as x = x + 1
+  console.log ("Printing out i = " + i);
+}
+//Nested loop
+for (var x = 0; x < 5; x = x + 1) {
+  for (var y = 0; y < 3; y = y + 1) {
+    console.log(x + "," + y);
+  }
+}
+
+// Increment and Decrement
+x++ or ++x // same as x = x + 1 
+x-- or --x // same as x = x - 1
+x += 3 // same as x = x + 3
+x -= 6 // same as x = x - 6
+x *= 2 // same as x = x * 2
+x /= 5 // same as x = x / 5
+
 //Functions
 
 
